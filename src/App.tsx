aef3f59c96ab2550438a3f080a21d760ba168c68
Aamin Khan <mail2aamin@gmail.com>
2022-03-26 00:14:05 +0530

    Full Screen added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import "./App.css";
 import Clock from "./components/Clock";
 
 function App() {
+  function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen;
+
+    if (!doc.fullscreenElement) {
+      requestFullScreen.call(docEl);
+    } else {
+      cancelFullScreen.call(doc);
+    }
+  }
   return (
     <div className="App dark">
       <Clock />
+      <button onClick={toggleFullScreen} className="fullscreen">
+        Full Screen
+      </button>
     </div>
   );
 }
